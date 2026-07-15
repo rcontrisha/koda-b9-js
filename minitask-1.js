cf67d835ed38425da136fa9b33029ec4824f0d5f
rcontrisha <rcontrisha@gmail.com>
2026-07-15 11:48:38 +0700

    first commit

---
+++ minitask-1.js
@@ -0,0 +1,23 @@
+// Soal Nomer 1
+const who = {
+    am: {
+        i: "Ridho Contrisha"
+    }
+}
+console.log(who.am.i)
+
+// Soal Nomer 2
+const hello = {
+    world: "Hello World"
+}
+console.log(hello.world)
+
+// Soal Nomer 3
+const obj = {
+    str: ["", ["", "", ["", "", "", {koda: [{tech: {academy: "Koda Next Academy"}}]}]]]
+}
+console.log(obj.str[1][2][3].koda[0].tech["academy"])
+
+// Soal Nomer 4
+const my = [{very: {best: ["", {sport: {is: "Footbal"}}]}}]
+console.log(my[0].very["best"][1].sport.is)
