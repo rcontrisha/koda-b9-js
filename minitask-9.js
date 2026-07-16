366d6631e279f609c3548ac00e7dba9414a7b761
rcontrisha <rcontrisha@gmail.com>
2026-07-16 17:13:32 +0700

    Day 1 - Minitask 9

---
+++ minitask-9.js
@@ -0,0 +1,27 @@
+// Master function
+function processArray (arr, processor) {
+    const result = []
+    
+    if (!Array.isArray(arr)) {
+        console.log('Input bukan array.')
+        return
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        result[i] = processor(element)
+    }
+
+    return result
+}
+
+// Callback function
+const showNumber = num => `Number: ${num}`
+const makeDollar = num => `$${num}`
+const addFive = num => num + 5
+
+const numbers = [1,2,3,4,5]
+
+console.log(processArray(numbers, showNumber))
+console.log(processArray(numbers, makeDollar))
+console.log(processArray(numbers, addFive))
